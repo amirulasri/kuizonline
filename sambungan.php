7b3d2f9dd00b191dd46b76060c28639be53cafb5
amirulasri <amirulasrix@gmail.com>
2021-04-22 23:36:57 +0800

    1.0

---
+++ sambungan.php
@@ -0,0 +1,11 @@
+<?php
+  $host = 'localhost';
+  $user = 'root';             
+  $password = '';       
+  $database = 'kuizonline';     
+
+  $sambungan = mysqli_connect($host, $user, $password, $database)
+  or die('Sambungan gagal');
+?>
+
+
---
+++ signup.php
@@ -0,0 +1,89 @@
+<?php
+include('sambungan.php'); 
+if(isset($_POST['idpelajar'])) {    
+    $idpelajar = $_POST["idpelajar"];
+    $namapelajar = $_POST["namapelajar"];
+    $idkelas = $_POST["idkelas"];
+    $password = $_POST["password"];
+    $sql = "insert into pelajar values('$idpelajar', '$namapelajar', '$idkelas', '$password')";
+    $result = mysqli_query($sambungan, $sql);
+    if ($result)
+        echo "<script>alert('Berjaya signup')</script>";
+    else 
+        echo "<script>alert('Tidak berjaya signup')</script>";
+    echo "<script>window.location='login.php'</script>";
+}
+
+//AUTO GENERATE ID UNTUK MURID
+$queryidmurid = mysqli_query($sambungan, "SELECT MAX(idpelajar) AS idpelajarterakhir FROM pelajar");
+$getdataidmurid = mysqli_fetch_assoc($queryidmurid);
+$idmuridkini = $getdataidmurid['idpelajarterakhir'];
+$buangpid = substr($idmuridkini,1);
+
+//TAMBAH 1 PADA ID YANG SEDIA ADA
+$murididbaru = $buangpid+1;
+
+//TUKARKAN INTEGER KEPADA STRING
+$stringidmurid = strval($murididbaru);
+
+//TAMBAH SEMULA P PADA MULA MULA ID BARU
+$idakhirbaru = 'P'.$stringidmurid;
+?> 
+
+<link rel="stylesheet" href="borang.css">
+<link rel="stylesheet" href="button.css">
+<body background=''>
+    <center>
+    <img src='tajuk1.png'><br>
+    <img src='tajuk2.png'>
+    </center>
+
+    <h3 class="panjang">SIGN UP</h3>
+    <form class="panjang" action="signup.php" method="post" name="borangdaftar">
+    <table>
+        <tr>
+            <td>ID Pelajar</td>
+            <td><input type="text" name="idpelajar" value="<?php echo $idakhirbaru; ?>" placeholder="P001  max 4 char"></td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><input type="text" name="namapelajar"></td>        
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>
+            <select name="idkelas">
+            <?php
+            $sql = "select * from kelas";
+            $data = mysqli_query($sambungan, $sql);
+            while ($kelas = mysqli_fetch_array($data)) {
+                echo "<option value='$kelas[idkelas]'>$kelas[namakelas]</option>";		
+            }
+            ?>
+            </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password" placeholder="max: 8 char"></td>
+        </tr>
+    </table>
+    <button class="tambah" type="submit" onclick="return periksakatalaluan(document.borangdaftar.password,8,8)">Daftar</button>
+    <button class="padam" type="button" onclick="window.location='login.php'">Batal</button>
+</form>
+<script>
+        function periksakatalaluan(katalaluan, aksaraminimum, aksaramaksimum) {
+            var inputkatalaluan = katalaluan.value;
+
+            if (inputkatalaluan.length < aksaraminimum) {
+                alert("Kata laluan yang anda masukkan kurang dari "+aksaraminimum+" aksara");
+                return false;
+            } else if (inputkatalaluan.length > aksaramaksimum){
+                alert("Kata laluan yang anda masukkan lebih dari "+aksaramaksimum+" aksara");
+                return false;
+            }else{
+                return confirm('Anda telah melengkapkan maklumat dengan betul. Klik OK untuk meneruskan pendaftaran, Klik Cancel untuk membatalkan pendaftaran');
+            }
+        }
+    </script>
+</body>
