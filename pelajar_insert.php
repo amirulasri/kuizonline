5669f06284e051c9dc547b4e0e6514e1eedc8cc2
amirulasri <amirulasrix@gmail.com>
2021-04-23 19:00:48 +0800

    Update

--- pelajar_insert.php
+++ pelajar_insert.php
@@ -1,21 +1,16 @@
 <?php
-    include("sambungan.php");
+include("sambungan.php");
 
-    $idpelajar = $_POST["idpelajar"];
-    $namapelajar = $_POST["namapelajar"];
-    $idkelas = $_POST["idkelas"];
-    $password = $_POST["password"];
+$idpelajar = $_POST["idpelajar"];
+$namapelajar = $_POST["namapelajar"];
+$idkelas = $_POST["idkelas"];
+$password = $_POST["password"];
 
-    $sql = "insert into pelajar values('$idpelajar', '$namapelajar', '$idkelas', '$password')";
-    
-    echo $sql;
+$sql = "insert into pelajar values('$idpelajar', '$namapelajar', '$idkelas', '$password')";
 
-    $result = mysqli_query($sambungan, $sql);
+$result = mysqli_query($sambungan, $sql);
 
-    if ($result == true)
-        echo "berjaya tambah pelajar";
-    else
-        echo "tidak berjaya tambah pelajar";
-?>
-
-
+if ($result == true)
+    echo "<script>alert('Pelajar berjaya ditambah'); window.location='pelajar_insert.html';</script>";
+else
+    echo "<script>alert('Gagal menambah pelajar'); window.location='pelajar_insert.html';</script>";
